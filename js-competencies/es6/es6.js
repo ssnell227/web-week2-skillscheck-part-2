6551d75c1f0fbf9740bdda305fd4499adaf7f3e6
ssnell227 <ssnell227@gmail.com>
2020-05-08 14:27:57 -0600

    finished es6

--- js-competencies/es6/es6.js
+++ js-competencies/es6/es6.js
@@ -10,9 +10,10 @@
 
 function addDucks(arr, ind) {
 
-  //WRITE YOUR FOR-LOOP HERE
-  //For your iterator, declare it with the let keyword, and name it "i"
-  
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] += 'Duck'
+  }
+
 
   //DO NOT TOUCH THIS
   return [arr, ind]
@@ -26,8 +27,7 @@ function addDucks(arr, ind) {
 //invocation within the if statement.
 
 if (true) {
-  //CODE HERE
-
+  let color = 'red'
 
   //DO NOT TOUCH THIS LINE
   getter.get(color)
@@ -38,7 +38,7 @@ if (true) {
 //and call it "greetingsEarthlings"
 //Assign it the value of "Hello".
 
-//Code here
+const greetingsEarthlings = 'Hello'
 
 
 //////////////////PROBLEM 4////////////////////
@@ -51,7 +51,9 @@ if (true) {
 //You MUST use a template string! If you need to
 //look it up, check out the documentation.
 
-//Code here
+const madLib = (string) => {
+  return `${string}, my dear Watson.`
+}
 
 
 //////////////////PROBLEM 5////////////////////
@@ -69,7 +71,7 @@ var obj = { a: "a", b: "b", c: "c" }
 //used spread operators.
 
 // var answers = ["Example A", "Example B", "Example C", "Example D"]
-// var answers = ["Example B", "Example C", "Example D"]
+var answers = ["Example B", "Example C", "Example D"]
 // var answers = ["Example A", "Example B"]
 // var answers = ["Example C", "Example D"]
 
@@ -79,7 +81,9 @@ var obj = { a: "a", b: "b", c: "c" }
 //take in a string, remove all the spaces, and return the
 //string without spaces.
 
-//Code here
+const cleanUp = (str) => {
+  return str.replace(/\s/g, '')
+}
 
 
 //////////////////PROBLEM 7////////////////////
@@ -89,9 +93,8 @@ var obj = { a: "a", b: "b", c: "c" }
 //Return ocean.
 
 function whatOcean(obj) {
-
-  //Code here
-  
+  let { ocean } = obj
+  return ocean
 }
 
 //////////////////PROBLEM 8////////////////////
@@ -104,4 +107,8 @@ function whatOcean(obj) {
 //plus the last element of the rest parameter, like so:
 //"spaghetti & liver with onions"
 
+const leftovers = (str, ...rest) => {
+  console.log(str, rest)
+  return `${str} & ${rest[rest.length-1]}`
+}
 
