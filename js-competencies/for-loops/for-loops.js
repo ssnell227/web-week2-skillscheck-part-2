86635056b5e9d679a1daa77bba7ac109ea44b2cf
ssnell227 <ssnell227@gmail.com>
2020-05-08 14:36:51 -0600

    finished for-loops

--- js-competencies/for-loops/for-loops.js
+++ js-competencies/for-loops/for-loops.js
@@ -10,8 +10,10 @@
 
   function addNumbers(value){
       var myNumbers = []
-      //Code here
-      
+      for (let i = 0; i < value; i++) {
+        myNumbers.push(i)
+      }
+      return myNumbers;
   }
 
 
@@ -24,8 +26,10 @@
 
   function backwardsGreeting(arr) {
     let greetings = [];
-    //Code here
-  
+    for (let i = arr.length -1; i >= 0; i--) {
+      greetings.push(`Hello, ${arr[i]}`)
+    }
+    return greetings
   }
 
 
@@ -37,6 +41,9 @@
 //the variable i for your for loop
 
   function leapFrog(arr) {
-    //Code here
+    for (let i = 0; i < arr.length; i+=2) {
+        arr[i] += ' frog'
+    }
+    return arr
 
   }
